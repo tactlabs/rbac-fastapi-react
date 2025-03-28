5f6dd4048edeadb54235ce09a88ddf976f8b3235
Soundarya Murugan <soundaryamurugan2002@gmail.com>
2025-03-28 12:05:23 +0530

    added basic files

---
+++ zzem.py
@@ -0,0 +1,17 @@
+
+# pylint: disable=missing-function-docstring, pointless-string-statement, missing-class-docstring
+
+'''
+@author: Raja CSP Raman
+
+source:
+    ?
+'''
+
+def startpy():
+
+    print("Tact101")
+
+
+if __name__ == '__main__':
+    startpy()
